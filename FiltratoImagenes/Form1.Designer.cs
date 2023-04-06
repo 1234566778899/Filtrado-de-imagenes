b9184ffe0032cde4805df41bdcc153bde66b5013
Carlos <ordazhoyos2001@gmail.com>
2023-04-06 14:18:47 -0500

    first commit

---
+++ FiltratoImagenes/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace FiltratoImagenes
+{
+    partial class imgOriginal
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.archivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.abrirImagenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.imgResultado = new System.Windows.Forms.PictureBox();
+            this.btnFiltrar = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.imgOriginal2 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imgResultado)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgOriginal2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.archivoToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1283, 30);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            
+            // 
+            // archivoToolStripMenuItem
+            // 
+            this.archivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.abrirImagenToolStripMenuItem});
+            this.archivoToolStripMenuItem.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
+            this.archivoToolStripMenuItem.Size = new System.Drawing.Size(88, 26);
+            this.archivoToolStripMenuItem.Text = "Archivo";
+            // 
+            // abrirImagenToolStripMenuItem
+            // 
+            this.abrirImagenToolStripMenuItem.Name = "abrirImagenToolStripMenuItem";
+            this.abrirImagenToolStripMenuItem.Size = new System.Drawing.Size(199, 26);
+            this.abrirImagenToolStripMenuItem.Text = "Abrir imagen";
+            this.abrirImagenToolStripMenuItem.Click += new System.EventHandler(this.abrirImagenToolStripMenuItem_Click);
+            // 
+            // imgResultado
+            // 
+            this.imgResultado.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.imgResultado.Location = new System.Drawing.Point(649, 96);
+            this.imgResultado.Name = "imgResultado";
+            this.imgResultado.Size = new System.Drawing.Size(595, 511);
+            this.imgResultado.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.imgResultado.TabIndex = 2;
+            this.imgResultado.TabStop = false;
+            // 
+            // btnFiltrar
+            // 
+            this.btnFiltrar.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFiltrar.Location = new System.Drawing.Point(411, 43);
+            this.btnFiltrar.Name = "btnFiltrar";
+            this.btnFiltrar.Size = new System.Drawing.Size(213, 33);
+            this.btnFiltrar.TabIndex = 3;
+            this.btnFiltrar.Text = "Filtrar";
+            this.btnFiltrar.UseVisualStyleBackColor = true;
+            this.btnFiltrar.Click += new System.EventHandler(this.btnFiltrar_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Media",
+            "Mediana",
+            "Laplaciano",
+            "Sovel"});
+            this.comboBox1.Location = new System.Drawing.Point(112, 45);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(293, 29);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(33, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 21);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Filtro:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(32, 610);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Imagen original";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(650, 610);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(125, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Imagen resultante";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(409, 572);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(213, 33);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Escala de Grises";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // imgOriginal2
+            // 
+            this.imgOriginal2.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.imgOriginal2.Location = new System.Drawing.Point(29, 97);
+            this.imgOriginal2.Name = "imgOriginal2";
+            this.imgOriginal2.Size = new System.Drawing.Size(595, 511);
+            this.imgOriginal2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.imgOriginal2.TabIndex = 9;
+            this.imgOriginal2.TabStop = false;
+            // 
+            // imgOriginal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1283, 649);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.btnFiltrar);
+            this.Controls.Add(this.imgResultado);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.imgOriginal2);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "imgOriginal";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imgResultado)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgOriginal2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem archivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem abrirImagenToolStripMenuItem;
+        private System.Windows.Forms.PictureBox imgResultado;
+        private System.Windows.Forms.Button btnFiltrar;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox imgOriginal2;
+    }
+}
+
---
+++ FiltratoImagenes/Form1.cs
@@ -0,0 +1,336 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FiltratoImagenes
+{
+    public partial class imgOriginal : Form
+    {
+        public imgOriginal()
+        {
+            InitializeComponent();
+        }
+
+        private void abrirImagenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "Archivos de imagen (*.jpg, *.jpeg, *.png, *.bmp)|*.jpg; *.jpeg; *.png; *.bmp";
+            openFileDialog1.Title = "Seleccionar imagen";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+
+                string imagePath = openFileDialog1.FileName;
+                imgOriginal2.ImageLocation = imagePath;
+                imgOriginal2.SizeMode = PictureBoxSizeMode.StretchImage;
+            }
+        }
+        private void ConvertirAEscaladeGrises(Bitmap imagenOriginal)
+        {
+            int promedio;
+            Color pixelColor;
+            int ancho = imagenOriginal.Width;
+            int alto = imagenOriginal.Height;
+            Bitmap imagenGrises = new Bitmap(ancho, alto);
+
+            for (int y = 0; y < alto; y++)
+            {
+                for (int x = 0; x < ancho; x++)
+                {
+                    pixelColor = imagenOriginal.GetPixel(x, y);
+                    promedio = (int)((pixelColor.R * 0.3) + (pixelColor.G * 0.59) + (pixelColor.B * 0.11));
+                    imagenGrises.SetPixel(x, y, Color.FromArgb(promedio, promedio, promedio));
+                }
+            }
+            imgOriginal2.Image = imagenGrises;
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            var frmCarga = new frmCarga();
+            frmCarga.Show();
+
+            // Ejecutar la función en segundo plano
+            await Task.Run(() => {
+                ConvertirAEscaladeGrises((Bitmap)imgOriginal2.Image);
+            });
+
+            frmCarga.Close();
+        }
+
+        private async void btnFiltrar_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.Text == "Media")
+            {
+                var frmCarga = new frmCarga();
+                frmCarga.Show();
+
+                // Ejecutar la función en segundo plano
+                await Task.Run(() => {
+                    imgResultado.Image = AplicarFiltroMedia((Bitmap)imgOriginal2.Image);
+                });
+
+                frmCarga.Close();
+            }else if (comboBox1.Text == "Mediana")
+            {
+                var frmCarga = new frmCarga();
+                frmCarga.Show();
+
+                // Ejecutar la función en segundo plano
+                await Task.Run(() => {
+                    imgResultado.Image = AplicarFiltroMediana((Bitmap)imgOriginal2.Image);
+                });
+
+                frmCarga.Close();
+
+            }
+            else if (comboBox1.Text == "Laplaciano")
+            {
+                var frmCarga = new frmCarga();
+                frmCarga.Show();
+
+                // Ejecutar la función en segundo plano
+                await Task.Run(() => {
+                    imgResultado.Image = AplicarFiltroLaplaciano((Bitmap)imgOriginal2.Image);
+                });
+
+                frmCarga.Close();
+            }
+            else
+            {
+                var frmCarga = new frmCarga();
+                frmCarga.Show();
+
+                // Ejecutar la función en segundo plano
+                await Task.Run(() => {
+                    imgResultado.Image = AplicarFiltroSobel((Bitmap)imgOriginal2.Image);
+                });
+
+                frmCarga.Close();
+            }
+        }
+
+        private void imgOriginal_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public Bitmap AplicarFiltroSobel(Bitmap imagen)
+        {
+            //Máscaras para detección de bordes en dirección X y Y
+            int[,] mascaraX = new int[,] {{-1,0,1},
+                                    {-2,0,2},
+                                    {-1,0,1}};
+
+            int[,] mascaraY = new int[,] {{-1,-2,-1},
+                                    {0,0,0},
+                                    {1,2,1}};
+
+            int tamanoMascara = 3;
+            Bitmap imagenResultado = new Bitmap(imagen.Width, imagen.Height);
+
+            for (int x = 1; x < imagen.Width - 1; x++)
+            {
+                for (int y = 1; y < imagen.Height - 1; y++)
+                {
+                    int valorNuevoPixelX = 0;
+                    int valorNuevoPixelY = 0;
+
+                    //Obtenemos los valores de los pixeles que forman la máscara X
+                    for (int i = -tamanoMascara / 2; i <= tamanoMascara / 2; i++)
+                    {
+                        for (int j = -tamanoMascara / 2; j <= tamanoMascara / 2; j++)
+                        {
+                            int pixelX = x + i;
+                            int pixelY = y + j;
+
+                            int valorMascaraX = mascaraX[i + tamanoMascara / 2, j + tamanoMascara / 2];
+
+                            if (pixelX >= 0 && pixelX < imagen.Width && pixelY >= 0 && pixelY < imagen.Height)
+                            {
+                                int valorPixelGrises = imagen.GetPixel(pixelX, pixelY).R;
+
+                                valorNuevoPixelX += valorPixelGrises * valorMascaraX;
+                            }
+                        }
+                    }
+
+                    //Obtenemos los valores de los pixeles que forman la máscara Y
+                    for (int i = -tamanoMascara / 2; i <= tamanoMascara / 2; i++)
+                    {
+                        for (int j = -tamanoMascara / 2; j <= tamanoMascara / 2; j++)
+                        {
+                            int pixelX = x + i;
+                            int pixelY = y + j;
+
+                            int valorMascaraY = mascaraY[i + tamanoMascara / 2, j + tamanoMascara / 2];
+
+                            if (pixelX >= 0 && pixelX < imagen.Width && pixelY >= 0 && pixelY < imagen.Height)
+                            {
+                                int valorPixelGrises = imagen.GetPixel(pixelX, pixelY).R;
+
+                                valorNuevoPixelY += valorPixelGrises * valorMascaraY;
+                            }
+                        }
+                    }
+
+                    //Calculamos el valor del nuevo pixel
+                    double valorNuevoPixel = Math.Sqrt(valorNuevoPixelX * valorNuevoPixelX + valorNuevoPixelY * valorNuevoPixelY);
+
+                    //Ajustamos el rango de valores
+                    if (valorNuevoPixel < 0)
+                    {
+                        valorNuevoPixel = 0;
+                    }
+                    else if (valorNuevoPixel > 255)
+                    {
+                        valorNuevoPixel = 255;
+                    }
+
+                    //Asignamos el valor al pixel actual en la imagen resultado
+                    imagenResultado.SetPixel(x, y, Color.FromArgb((int)valorNuevoPixel, (int)valorNuevoPixel, (int)valorNuevoPixel));
+                }
+            }
+            return imagenResultado;
+        }
+        public Bitmap AplicarFiltroLaplaciano(Bitmap imagen)
+        {
+            int[,] mascara = new int[,]{{-1,-1,-1},
+                                  {-1, 8,-1},
+                                  {-1,-1,-1}};
+
+            int tamanoMascara = 3;
+            Bitmap imagenResultado = new Bitmap(imagen.Width, imagen.Height);
+
+            for (int x = 1; x < imagen.Width - 1; x++)
+            {
+                for (int y = 1; y < imagen.Height - 1; y++)
+                {
+                    int valorNuevoPixel = 0;
+
+                    //Obtenemos los valores de los pixeles que forman la máscara
+                    for (int i = -tamanoMascara / 2; i <= tamanoMascara / 2; i++)
+                    {
+                        for (int j = -tamanoMascara / 2; j <= tamanoMascara / 2; j++)
+                        {
+                            int pixelX = x + i;
+                            int pixelY = y + j;
+
+                            int valorMascara = mascara[i + tamanoMascara / 2, j + tamanoMascara / 2];
+
+                            if (pixelX >= 0 && pixelX < imagen.Width && pixelY >= 0 && pixelY < imagen.Height)
+                            {
+                                int valorPixelGrises = imagen.GetPixel(pixelX, pixelY).R;
+
+                                valorNuevoPixel += valorPixelGrises * valorMascara;
+                            }
+                        }
+                    }
+
+                    //Ajustamos el rango de valores
+                    if (valorNuevoPixel < 0)
+                    {
+                        valorNuevoPixel = 0;
+                    }
+                    else if (valorNuevoPixel > 255)
+                    {
+                        valorNuevoPixel = 255;
+                    }
+
+                    //Asignamos el valor al pixel actual en la imagen resultado
+                    imagenResultado.SetPixel(x, y, Color.FromArgb(valorNuevoPixel, valorNuevoPixel, valorNuevoPixel));
+                }
+            }
+            return imagenResultado;
+        }
+        public Bitmap AplicarFiltroMediana(Bitmap imagen)
+        {
+            int tamanoMascara = 3;
+            Bitmap imagenResultado = new Bitmap(imagen.Width, imagen.Height);
+
+            for (int x = 0; x < imagen.Width; x++)
+            {
+                for (int y = 0; y < imagen.Height; y++)
+                {
+                    List<int> valoresPixel = new List<int>();
+
+                    //Obtenemos los valores de los pixeles que forman la máscara
+                    for (int i = -tamanoMascara / 2; i <= tamanoMascara / 2; i++)
+                    {
+                        for (int j = -tamanoMascara / 2; j <= tamanoMascara / 2; j++)
+                        {
+                            int pixelX = x + i;
+                            int pixelY = y + j;
+
+                            if (pixelX >= 0 && pixelX < imagen.Width && pixelY >= 0 && pixelY < imagen.Height)
+                            {
+                                valoresPixel.Add(imagen.GetPixel(pixelX, pixelY).R);
+                            }
+                        }
+                    }
+
+                    //Ordenar los valores de menor a mayor
+                    valoresPixel.Sort();
+
+                    //Obtener el valor mediano
+                    int valorMediano = valoresPixel[valoresPixel.Count / 2];
+
+                    //Asignamos el valor mediano al pixel actual en la imagen resultado
+                    imagenResultado.SetPixel(x, y, Color.FromArgb(valorMediano, valorMediano, valorMediano));
+                }
+            }
+            return imagenResultado;
+        }
+        public Bitmap AplicarFiltroMedia(Bitmap bmp)
+        {
+            if (bmp == null)
+                throw new ArgumentNullException("bmp");
+
+            Bitmap filteredBmp = new Bitmap(bmp.Width, bmp.Height);
+
+            int filterSize = 3; // Tamaño del filtro
+            int[] pixels = new int[filterSize * filterSize];
+
+            // Iteramos sobre todos los píxeles de la imagen
+            for (int y = 0; y < bmp.Height; y++)
+            {
+                for (int x = 0; x < bmp.Width; x++)
+                {
+                    // Recorremos los píxeles alrededor del actual para aplicar el filtro de media
+                    int index = 0;
+                    for (int j = -filterSize / 2; j <= filterSize / 2; j++)
+                    {
+                        for (int i = -filterSize / 2; i <= filterSize / 2; i++)
+                        {
+                            int px = x + i;
+                            int py = y + j;
+
+                            if (px < 0 || px >= bmp.Width || py < 0 || py >= bmp.Height)
+                                continue;
+
+                            // Obtenemos el valor del pixel en escala de grises
+                            Color c = bmp.GetPixel(px, py);
+                            int grayValue = (int)(0.2989 * c.R + 0.5870 * c.G + 0.1140 * c.B);
+
+                            pixels[index++] = grayValue;
+                        }
+                    }
+
+                    // Ordenamos los valores de los píxeles y tomamos el valor central
+                    Array.Sort(pixels);
+                    int medianValue = pixels[(filterSize * filterSize) / 2];
+
+                    // Aplicamos el valor de la mediana al pixel actual
+                    filteredBmp.SetPixel(x, y, Color.FromArgb(medianValue, medianValue, medianValue));
+                }
+            }
+
+            return filteredBmp;
+        }
+    }
+}
